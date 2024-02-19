5c37ee70e5bf7f06ec1f753b4e5a4fdf5584829c
MercyNJ <mercinjuguna@gmail.com>
2024-02-19 14:42:16 +0000

    Members CRUD Operations and their associated templates

--- members/members_views.py
+++ members/members_views.py
@@ -6,3 +6,79 @@ import models
 from models.members import Members
 from models.issuance import Issuance
 from models import storage
+
+@members_bp.route('/members', methods=['GET'])
+def view_members():
+    """list all the library members in database"""
+    try:
+        members = storage.all(Members).values()
+        members = sorted(members, key=lambda k: k.id)
+        return render_template(
+            'view_members.html', members=members)
+    except Exception as e:
+        return "Error fetching member data: {}".format(str(e))
+
+@members_bp.route(
+    '/add_members_form', methods=['GET', 'POST'],
+    strict_slashes=False)
+@login_required
+def add_members_form():
+    """allows the librarian to add member to the record"""
+    if request.method == 'POST':
+        try:
+            members_name = request.form.get('members_name')
+            members_email = request.form.get('members_email')
+            members_contact = request.form.get('members_contact')
+            new_member = Members(
+                name=members_name, email=members_email,
+                contact=members_contact)
+            new_member.save()
+            return redirect(url_for('members.view_members'))
+        except Exception as e:
+            return "Error: {}".format(str(e))
+    return render_template('add_members_form.html')
+
+
+
+@members_bp.route(
+    '/update_members_form/<members_id>',
+    methods=['GET', 'POST'])
+@login_required
+def update_members_form(members_id):
+    """allow update of member already existing data"""
+    members_id = int(members_id)
+    member = storage.get(Members, members_id)
+    if request.method == 'POST':
+        try:
+            if member:
+                member.name = request.form.get('members_name')
+                member.email = request.form.get('members_email')
+                member.contact = request.form.get('members_contact')
+                member.save()
+                return redirect(url_for('members.view_members'))
+            else:
+                return "Member not found"
+        except Exception as e:
+            return "Error: {}".format(str(e))
+    elif request.method == 'GET':
+        if member:
+            return render_template(
+                'update_members_form.html', member=member)
+        else:
+            return "Member not found"
+
+
+
+@members_bp.route('/delete_member/<member_id>', methods=['GET', 'POST'])
+@login_required
+def delete_member(member_id):
+    """deletes a record of a member"""
+    try:
+        member = storage.get(Members, int(member_id))
+        if member:
+            storage.delete(member)
+            storage.save()
+        return redirect(url_for('members.view_members'))
+    except Exception as e:
+        return "Error: {}".format(str(e))
+
--- user/user_views.py
+++ user/user_views.py
@@ -13,3 +13,145 @@ from models import storage
 from models.user import User
 import os
 import shortuuid
+
+user_ids = [1, 2, 3, 4]
+max_length = 20
+
+
+class LoginForm(FlaskForm):
+    """defines a class for loggining in"""
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    login = SubmitField('Login')
+    remember = BooleanField('Remember Me')
+
+
+class ForgotPasswordForm(FlaskForm):
+    """defines a class to change password"""
+    username = StringField('Username', validators=[DataRequired()])
+    new_password = PasswordField('New Password', validators=[DataRequired()])
+    forgot_password = SubmitField('Submit New Password')
+
+
+class ChangePasswordForm(FlaskForm):
+    old_password = PasswordField('Old Password', validators=[DataRequired()])
+    changed_password = PasswordField('Changed Password', validators=[DataRequired()])
+    change_password = SubmitField('Change Password')
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    """fetches the user from the database"""
+    return storage.get(User, int(user_id))
+
+
+
+@user_bp.route('/login', methods=['GET', 'POST'])
+def login():
+    """This facilitates the user login process"""
+    login_form = LoginForm()
+    forgot_password_form = ForgotPasswordForm()
+
+    if login_form.validate_on_submit():
+        username = login_form.username.data
+        password = login_form.password.data
+        user_found = False
+        for user_id in user_ids:
+            user = load_user(user_id)
+            if user and username == user.username:
+                user_found = True
+                if check_password_hash(user.password, password):
+                    login_user(user, remember=login_form.remember.data)
+                    return redirect(url_for('user.index'))
+                else:
+                    flash("Password mismatch")
+                    break
+
+        if not user_found:
+            flash("Incorrect username")
+
+    return render_template('login.html', login_form=login_form)
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    return redirect(url_for('user.login'))
+
+
+@user_bp.route('/forgot_password', methods=['GET', 'POST'])
+# @login_required
+def forgot_password():
+    """"This allows the user to change the password"""
+    forgot_password_form = ForgotPasswordForm()
+    if forgot_password_form.validate_on_submit():
+        username = forgot_password_form.username.data
+        new_password = forgot_password_form.new_password.data
+        hashed_password = generate_password_hash(new_password)
+        users = storage.all(User).values()
+        for user in users:
+            if user.username == username:
+                user.password = hashed_password
+                user.save()
+                flash("Password changed successfully")
+                return redirect(url_for('user.login'))
+    return render_template(
+        'forgot_password.html', forgot_password_form=forgot_password_form)
+
+
+@user_bp.route('/change_password', methods=['GET', 'POST'])
+@login_required
+def change_password():
+    """"This allows the user to change the password"""
+    change_password_form = ChangePasswordForm()
+    if change_password_form.validate_on_submit():
+        old_password = change_password_form.old_password.data
+        new_password = change_password_form.changed_password.data
+        user = current_user
+        if user and check_password_hash(user.password, old_password):
+            user.set_password(new_password)
+            user.save()
+            flash("Password changed successfully")
+            return redirect(url_for('user.index'))
+        else:
+            flash("Invalid old password. Please try again.")
+    return render_template(
+        'change_password.html', change_password_form=change_password_form)
+
+
+@user_bp.route('/')
+@login_required
+def index():
+    """Displays the home page of the website after the user has logged in"""
+    return render_template(
+        'index.html', user=current_user)
+
+
+@user_bp.route('/members_listing')
+@login_required
+def members_listing():
+    return redirect(url_for('members.view_members'))
+
+
+@user_bp.route('/books_listing')
+@login_required
+def books_listing():
+    return redirect(url_for('books.view_books'))
+
+
+@user_bp.route('/issuance_listing')
+@login_required
+def issuance_listing():
+    return redirect(url_for('issuance.view_issuances'))
+
+
+@user_bp.route('/statement_listing')
+@login_required
+def statement_listing():
+    return redirect(url_for('statement.view_open_issuances'))
+
+
+@user_bp.route('/logout')
+@login_required
+def logout():
+    """Logs out the user"""
+    logout_user()
+    return redirect(url_for('user.login'))
